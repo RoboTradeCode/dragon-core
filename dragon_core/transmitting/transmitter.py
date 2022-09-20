04493d74bcbeb7bfbfc27e404f9491e4ca23a6bb
Khanbekov <khanbekov.blockchain@gmail.com>
2022-09-20 12:30:05 +0300

    add exceptions handling

--- dragon_core/transmitting/transmitter.py
+++ dragon_core/transmitting/transmitter.py
@@ -1,5 +1,9 @@
+import logging
+
 import orjson as orjson
-from aeron import Publisher
+from aeron import Publisher, AeronPublicationNotConnectedError, AeronPublicationAdminActionError, AeronPublicationError
+
+logger = logging.getLogger(__name__)
 
 
 class Transmitter(object):
@@ -11,6 +15,22 @@ class Transmitter(object):
         self._publisher = Publisher(channel=channel, stream_id=stream_id)
 
     def publish(self, message: dict):
+        is_successful = False
         message_as_str = str(orjson.dumps(message))
-        self._publisher.offer(message_as_str)
+        while not is_successful:
+            try:
+                self._publisher.offer(message_as_str)
+                is_successful = True
+
+            # обработка случая, когда нет подписчика
+            except AeronPublicationNotConnectedError:
+                logger.warning(f'Subscriber is not connected. Message: {message_as_str}')
+                break
+            # обработка случая admin actin (сообщение будет отправлено снова)
+            except AeronPublicationAdminActionError:
+                continue
+            # обработка прочих ошибок aeron
+            except AeronPublicationError as e:
+                logger.warning(f'Error on aeron publishing: {e}')
+                break
 
