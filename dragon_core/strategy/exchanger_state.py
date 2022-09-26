f7d28d3872b08349dccc754154354d55cdb63a7f
Khanbekov <khanbekov.blockchain@gmail.com>
2022-09-26 20:39:45 +0300

    add to_dict method

--- dragon_core/strategy/exchanger_state.py
+++ dragon_core/strategy/exchanger_state.py
@@ -5,6 +5,16 @@ import simplejson
 
 
 @dataclasses.dataclass
+class CoreOrder:
+    sell_market_order_price: Decimal = None
+    buy_limit_order_price: Decimal = None
+    buy_market_order_price: Decimal = None
+    sell_limit_order_price: Decimal = None
+    buy_profit: Decimal = None
+    sell_profit: Decimal = None
+
+
+@dataclasses.dataclass
 class ExchangeState:
     """
     Снимок состояния биржи, включающий всю необходимую актуальную информацию, но без исторической информации.
@@ -13,10 +23,13 @@ class ExchangeState:
     limit_orders: dict[str, dict] = None
     orderbook: dict = None
     balance: dict = None
-    sell_market_order_price: Decimal = None
-    buy_limit_order_price: Decimal = None
-    buy_market_order_price: Decimal = None
-    sell_limit_order_price: Decimal = None
-    buy_profit: Decimal = None
-    sell_profit: Decimal = None
+    core_orders: dict[str, CoreOrder] = None
 
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'limit_orders': self.limit_orders,
+            'orderbook': self.orderbook,
+            'balance': self.balance,
+            'core_orders': list(self.core_orders.values())
+        }
